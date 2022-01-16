1eb9e059ae9ca21c9bc94ec1ed2ab12821cdde8f
Taylor <tayyiorrobinson@gmail.com>
2022-01-16 21:43:44 +0000

    [gha] create a workflow

---
+++ .github/versionUpdate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+PKGVERSION=$(cat ./package.json | grep version | cut -d '"' -f 4)
+PKGNAME=$(cat ./package.json | grep name | cut -d '"' -f 4)
+GIT_COMMIT=$(git rev-parse --short HEAD)
+GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD )
+ROJO_VERSION=$(./rojo -V)
+TSC_VERSION=$(rbxtsc --version)
+HOSTNAME=$(hostname)
+UNAME=$(uname -sr)
+echo "Building $PKGNAME@$PKGVERSION ($GIT_BRANCH@$GIT_COMMIT) (with $ROJO_VERSION & rbxtsc $TSC_VERSION (node $(node -v)) on $HOSTNAME running $UNAME)"
+echo "export {};print(\`$PKGNAME@$PKGVERSION ($GIT_BRANCH#$GIT_COMMIT)" > ./src/client/version.client.ts
+echo "built with $ROJO_VERSION & rbxtsc $TSC_VERSION (node $(node -v))" >> ./src/client/version.client.ts
+echo "on $HOSTNAME running $UNAME\`);" >> ./src/client/version.client.ts
---
+++ src/client/version.client.ts
@@ -0,0 +1,3 @@
+export {};print(`roblox-wordle-clone@1.0.0 (master#5eb6d67)
+built with  & rbxtsc 1.2.7 (node v17.3.0)
+on vulpes.i running Darwin 21.2.0`);
